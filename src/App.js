41278195f5b43413922eff9139b7e5d3ac26495e
x200 <blueredcat@gmail.com>
2017-08-24 18:17:28 +1000

    just before testing with inferno

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
 import './static/css/app.css'
-import { Textfit } from 'react-textfit'
+import BigText from 'react-bigtext'
+
+var bigTextOptions = {
+    height: 100
+}
 
 class App extends Component {
     render() {
         return (
-          <div className="appMainDiv">
-              <Textfit
-                  mode="single"
-              >
-                  <div className="headingContainer">
-                      <div className="appH2"> Ben Litwin Photography </div>
+            <div className="appMainDiv">
+                
+                  <div className="pageContainer">
+                     
+                        <div className="appH2"> 
+                            
+                            <BigText
+                                options={bigTextOptions}
+                            > 
+                                Ben Litwin Photography  
+                            </BigText>
+                        
+                        </div>
+                     
                   </div>
-              </Textfit>
-          </div>
-        );
+            </div>
+        )
     }
 }
 
